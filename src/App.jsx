1cd8f550d2dfc4b2725d037491afeed39bd9edcd
NaveenLakmal <naveenlakmal.edu@gmail.com>
2024-02-25 08:41:10 +0530

    *inital commit *create react project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+function App() {
+  
+
+  return (
+    <>
+      <button className="btn btn-primary">atta</button>
+        
+    </>
+  )
+}
+
+export default App
